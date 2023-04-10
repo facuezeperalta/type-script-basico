d3d03195fd18c0dd018e3b9fe57c0704be08c33a
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-04-10 17:28:11 -0300

    tsconfig agregado, y hasta clase 5

---
+++ src/04-numbers.ts
@@ -0,0 +1,13 @@
+(()=>{
+let productPrice = 100;
+productPrice = 12;
+console.log('Producto Price',productPrice);
+
+let customerAge = 28;
+customerAge = customerAge + 1;
+console.log('Customer Age',customerAge);
+
+let productInStock: number; //si no inicializo la variable tengo que asignar el tipo si o si.
+console.log(productInStock);
+
+})();
