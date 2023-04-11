eadbaaa4ab9a251f304b174f3e6a9ce3b80383a0
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-04-11 01:22:30 -0300

    arrays

---
+++ src/07-Arrays.ts
@@ -0,0 +1,16 @@
+(()=>{
+
+  //forma implicita
+  let prices = [1,2,1,3,4,120,40,'Hola',true]; // es un array.
+  prices.push(1340);
+
+  //forma declarada en donde coloco lo que quiero dentro del array.
+
+let mixed:(number | string | boolean | Object)[] = ['hola',true];
+
+
+
+
+
+
+})();
